23d26b870ff9f3b42dbb42483231958f1ec98047
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-08-11 17:50:06 -0700

    adding psudo coding for html routing pt 1

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,17 +1,30 @@
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+//path exits in models can I use it here?
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
+  // at route / get and render index.handlebars page
+  // Example is a var found in example.js I believe this provides the sequlized database data??
+  // Still not sure where msg: "welcome come into play"
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
         msg: "Welcome!",
+        // examples is the handlebar variable name for our database dbExamples
         examples: dbExamples
       });
     });
   });
+  //Do we need similar app.get requests for each of the handlebar or html pages?
+  // Ex: one for homepage/index, a second for a all posts page,
+  // a third for a create a post page, a fourth for a admin's page?
 
-  // Load example page and pass in an example by id
+
+  // Load example.handlebars page and pass in an example by id
+  // my discription: 
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(
       dbExample
