d48c5c917db7ff8e4eff9088f5cd14d8a6167fd3
armjim14 <ajimenez1497@gmail.com>
2019-08-17 17:14:29 -0700

    connected all tables

---
+++ models/commets.js
@@ -0,0 +1,17 @@
+module.exports = function (sequelize, DataTypes) {
+
+    var comments = sequelize.define("comments", {
+        comment: DataTypes.STRING,
+    });
+
+    comments.associate = function(models) {
+        comments.belongsTo(models.posts, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
+    return comments
+
+};
