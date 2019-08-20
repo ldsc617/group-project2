0b0a8565acef6cc4cbb82ead9f2626a9b6fd42dd
armjim14 <ajimenez1497@gmail.com>
2019-08-19 20:00:09 -0700

    attempting to merge

--- models/commets.js
+++ models/commets.js
@@ -1,22 +1,20 @@
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
+  var comments = sequelize.define("comments", {
+    comment: DataTypes.STRING
+  });
 
-    var comments = sequelize.define("comments", {
-        comment: DataTypes.STRING,
+  comments.associate = function(models) {
+    comments.belongsTo(models.posts, {
+      foreignKey: {
+        allowNull: false
+      }
     });
+    comments.belongsTo(models.Users, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
 
-    comments.associate = function(models) {
-        comments.belongsTo(models.posts, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-        comments.belongsTo(models.Users, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    };
-
-    return comments
-
+  return comments;
 };
--- visuals/app.js
+++ visuals/app.js
@@ -1,9 +1,8 @@
-
-const canvas = d3.select('.canva');
+var canvas = d3.select(".canva");
 
 // var dataArray = [4, 15, 34, 123, 23, 0];
 
-// JSON - Javascript Object Notation 
+// JSON - Javascript Object Notation
 // var dataArray = [
 //     {
 //         width: 25,
@@ -19,37 +18,39 @@ const canvas = d3.select('.canva');
 
 // ];
 
-d3.json('text.json')
-    .then(data => {
-        console.log(data);
-        // code here to actually draw elements on screen 
+d3.json("text.json").then(function(data) {
+  console.log(data);
+  // code here to actually draw elements on screen
 
-        // add an svg element 
-        const svg = canvas.append('svg')
-            .attr('width', '600')
-            .attr('height', '600');
+  // add an svg element
+  var svg = canvas
+    .append("svg")
+    .attr("width", "600")
+    .attr("height", "600");
 
-        const rect = svg.selectAll('rect');
+  var rect = svg.selectAll("rect");
 
-        rect.data(data)
-            .enter().append('rect')
-            .attr('width', d => d.width * 0.7)
-            .attr('fill', (d, i) => d.fill)
-            .attr('height', function (d) {
-                return d.height * 1;
-            })
-            .attr('x', function (d, i) {
-                console.log(d);
-                return i * 18;
-            })
-            .attr('rx', 2.5)
-            .attr('y', function (d, i) {
-                return 200 - (d.height * 1);
-            })
-
-        console.log(rect);
-
-
+  rect
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("width", function(d) {
+      d.width * 0.7;
     })
+    .attr("fill", function(d) {
+      d.fill;
+    })
+    .attr("height", function(d) {
+      return d.height * 1;
+    })
+    .attr("x", function(d, i) {
+      console.log(d);
+      return i * 18;
+    })
+    .attr("rx", 2.5)
+    .attr("y", function(d) {
+      return 200 - d.height * 1;
+    });
 
-
+  console.log(rect);
+});
